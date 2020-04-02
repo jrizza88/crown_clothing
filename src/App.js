82a8d7881e9779c8197555a645a356abc9cf60aa
Jamar Torres <Jetorres88@gmail.com>
2020-04-01 20:06:02 -0400

    added files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HomePage from "./Components/HomePageComponent";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
