b4a9159a328682e6da0554e04896538bee602475
Jamar Torres <Jetorres88@gmail.com>
2020-04-08 22:53:13 -0400

    added hover effects to the menu options. The styling is finished for the MenuItem component

--- src/Components/MenuItem/Menu-item.js
+++ src/Components/MenuItem/Menu-item.js
@@ -2,9 +2,10 @@ import React from 'react';
 import "./Menu-item.scss";
 
 const MenuItem = ({title, imageUrl, size}) => (
-<div style={{
+<div className={`${size} menu-item`}>
+    <div className="background-image" style={{
     backgroundImage: `url(${imageUrl})`
-}}className={`${size} menu-item`}>
+}}/>
     <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">Shop Now</span>
