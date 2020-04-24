f46f6f06eec875581ca676e5e283c9c216588de3
Jamar Torres <Jetorres88@gmail.com>
2020-04-23 23:47:23 -0400

    added unsubscribe Auth method to ensure the closure of a subscription

--- src/App.js
+++ src/App.js
@@ -17,12 +17,18 @@ class App extends React.Component {
     }
   }
 
+  unsubscribeFromAuth = null
+
   componentDidMount(){
-    auth.onAuthStateChanged(user => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
       this.setState({ currentUser: user})
 
       console.log('user: ', user)
     })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
   }
 
   render(){
